ed2fed01284bc41f02b2926957db485941a98564
Madiyar Yp <madimadiar96@gmail.com>
2023-09-04 11:07:21 +0600

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public void errorInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        error1.errorInfo();
+        Error error2 = new Error(true, 1, "HTTP Status 403 for request POST");
+        error2.errorInfo();
+        Error error3 = new Error(true, 2, "Resource not accessible by integration");
+        error3.errorInfo();
+    }
+}
