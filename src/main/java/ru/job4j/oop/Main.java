75ff8aba75907362444f37fdc66dc87a1b418b37
Madiyar Yp <madimadiar96@gmail.com>
2023-09-06 15:46:30 +0600

    6.0. Абстрактные классы [#504949 #442855]

--- src/main/java/ru/job4j/oop/Main.java
+++ src/main/java/ru/job4j/oop/Main.java
@@ -2,7 +2,8 @@ package ru.job4j.oop;
 
 public class Main {
     public static void main(String[] args) {
-        Truck truck = new Truck();
+        Truck truck = new Truck("Volvo", 200);
+        truck.info();
         truck.accelerate();
         truck.steer();
         truck.brake();
