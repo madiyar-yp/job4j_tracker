81eda26394982d64c12bdd1c5530901cd5511b81
Madiyar Yp <madimadiar96@gmail.com>
2023-09-07 09:09:45 +0600

    6.0. Абстрактные классы [#504949 #442855]

--- src/main/java/ru/job4j/oop/Main.java
+++ src/main/java/ru/job4j/oop/Main.java
@@ -2,8 +2,7 @@ package ru.job4j.oop;
 
 public class Main {
     public static void main(String[] args) {
-        Truck truck = new Truck("Volvo", 200);
-        truck.info();
+        Truck truck = new Truck();
         truck.accelerate();
         truck.steer();
         truck.brake();
--- src/main/java/ru/job4j/oop/Truck.java
+++ src/main/java/ru/job4j/oop/Truck.java
@@ -1,18 +1,6 @@
 package ru.job4j.oop;
 
 public class Truck extends Vehicle {
-    public Truck() { }
-
-    public Truck(String brand, int maxSpeed) {
-        this.brand = brand;
-        this.maxSpeed = maxSpeed;
-    }
-
-    public void info() {
-        System.out.println("Бренд: " + this.brand);
-        System.out.println("Максимальная скорость: " + this.maxSpeed);
-    }
-
     @Override
     public void accelerate() {
         System.out.println("Электронная педаль газа");
